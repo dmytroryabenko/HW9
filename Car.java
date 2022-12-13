dd8569d1690c6ad425958c8282652eabffe88fdf
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-12-13 10:52:48 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -24,36 +24,21 @@ public class Car {
     public UUID getId() {
         return id;
     }
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
+  
     public Brand getBrand() {
         return brand;
     }
-    public void setBrand(Brand brand) {
-        this.brand = brand;
-    }
-
+    
     public int getYear() {
         return year;
     }
-    public void setYear(int year) {
-        this.year = year;
-    }
-
+   
     public int getMileage() {
         return mileage;
-    }
-    public void setMileage(int mileage) {
-        this.mileage = mileage;
     }
 
     public int getPrice() {
         return price;
-    }
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     @Override
